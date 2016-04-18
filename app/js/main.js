861bf82d769d6f4ab10c19f6101098333e9a794b
alexeymasnoy <masnoy0alexey@gmail.com>
2016-04-18 09:46:41 +0300

    Тестовое задание LoftSchool final

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,37 @@
+"use strict";
+
 $(document).ready(function() {
 
-
-	$("input[type='checkbox']").click(function() {
+	$(".checkbox").click(function() {
 		$(this).parents(".course_item")
-			.delay(1000)
-			.append('<div class="shade"></div>')
-			.css("box-shadow","0px 0px 0px #fff")
-			.delay(1000)
-			.fadeOut(300, function(){
+			   .append('<div class="shade"></div>')
+			   .css("box-shadow","0px 0px 0px #fff")
+			   .delay(1000)
+			   .fadeOut(300, function(){
                 $(this).remove();
 
-if ($('.course_item').length > 0)	{
-		console.log($('.course_item').length)
-    } 
-    else {
+			if ($('.course_item').length > 0)	{
+					console.log($('.course_item').length)
+			 } 
+		    else {
+				$('.course_items').append('<div class="course_complete"><h1>&#10003 Задание выполненно</h1></div>')
+				}
 
-	$('.course_items').append('<div class="course_complete"><h1>&#10003 Задание выполненно</h1></div>')
+        	});
+		$(this).next().css('color', '#8cc34b');
+	    	});
 	
-	}
-
-});
-	
-	$(this).next()
-	        .css('color', '#8cc34b');
-               
-    
-		
-
-});
-	
-	$('article.course_item h3').hover(function(){
-		$(this).append('<div class="line"></div>')
-				.css({"color": "#8cc34b",
-					  "cursor": "pointer"});
-	}, function() {
-		$(this).css("color", "#727272")
-			   .find(".line:last")
-			   .remove();
+		$('article.course_item').hover(function(){
+			$(this).append('<div class="line"></div>')
+				   .children('h3')
+				   .css({"color": "#8cc34b",
+					     "cursor": "pointer"});
+				   
+				}, function() {
+					$(this).children('h3').css("color", "#727272");
+			   		$(this).find(".line:last").remove();
+			   		
 	});
-});
+});			   			   
+			   			   	   
+			   			   
